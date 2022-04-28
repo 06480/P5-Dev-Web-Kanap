c3985563b665be2ce20071dbf9b8ace9fe7ca87a
06480 <paul.messiant@gmail.com>
2022-04-28 16:12:24 +0200

    modifs

--- back/routes/script.js
+++ back/routes/script.js
@@ -1,2 +1,16 @@
 let elt = index.getElementsById('items')
+
 elt.innerHTML = "<ul><li>Elément 1</li><li>Elément 2</li></ul>";
+
+export function APIwork(){
+    fetch("http://localhost:3000/api/products")
+    .then(function(res){
+      if (res.ok){
+        return res.json();
+      }
+    })
+    .then (function(value){
+      console.log(value);
+    })
+    .catch(function(err){})
+}
