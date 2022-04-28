51360e94756122894e9f9c0dbd8ab9ed70e4f64f
06480 <paul.messiant@gmail.com>
2022-04-28 16:09:44 +0200

    modifs

---
+++ back/routes/script.js
@@ -0,0 +1,2 @@
+let elt = index.getElementsById('items')
+elt.innerHTML = "<ul><li>Elément 1</li><li>Elément 2</li></ul>";
--- back/server.js
+++ back/server.js
@@ -43,3 +43,19 @@ server.on('listening', () => {
 });
 
 server.listen(port);
+
+export function APIwork(){
+    fetch("http://localhost:3000/api/products")
+    .then(function(res){
+      if (res.ok){
+        return res.json();
+      }
+    })
+    .then (function(value){
+      console.log(value);
+    })
+    .catch(function(err){})
+}
+
+let elt = index.getElementsById('items')
+elt.innerHTML = "<ul><li>Elément 1</li><li>Elément 2</li></ul>";
