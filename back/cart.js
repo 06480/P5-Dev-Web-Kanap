295699a7bf3e979a8bc9c4c770e57e538abc5578
06480 <paul.messiant@gmail.com>
2022-05-09 12:50:45 +0200

    modifs

---
+++ back/cart.js
@@ -0,0 +1,3 @@
+function change(event){
+    console.log(event)
+}
--- back/product.js
+++ back/product.js
@@ -1,39 +1,58 @@
 (async function() {
-    const productId = getProductId()
-    const product = await getProduct(productId)
-    displayProduct(product)
+    const productId = getProductId();
+    let product = await getProduct(productId);
+    displayProduct(product);   
 })()
 
+// let product = [];
+
 function getProductId() {
-    return new URL(location.href).searchParams.get("id")
+    return new URL(location.href).searchParams.get("id");
 }
 
 function getProduct(productId){
     return fetch(`http://localhost:3000/api/products/${productId}`)
         .then(function(httpBodyResponse) {
-            return httpBodyResponse.json()
+            return httpBodyResponse.json();
         })
         .then(function(products){
-            return products
+            return products; 
         })
         .catch(function(error){
-            alert(error)
+            alert(error);
         })
-}
+};
 
 function displayProduct(product){
-    document.getElementById("title").textContent = product.name
-    document.getElementById("price").textContent = product.price
-    document.getElementById("productImg").src = product.imageUrl
-    document.getElementById("description").textContent = product.description
+    document.getElementById("title").textContent = product.name;
+    document.getElementById("price").textContent = product.price;
+    document.getElementById("productImg").src = product.imageUrl;
+    document.getElementById("description").textContent = product.description;
 
     let colorsOptions = '';
     for(let i = 0; i < product.colors.length; i++){
-        colorsOptions += `<option id="color1" value="color">${product.colors[i]}</option>`
-    }
+        colorsOptions += `<option id="color1" value="color">${product.colors[i]}</option>`;
+    };
     document.getElementById("colors").innerHTML = colorsOptions;
+    document.getElementById("item__content__addButton").innerHTML = `
+    <button id="${product._id}">Ajouter au panier</button>`;
+
+    addBastket(product);
+};
+
+
+function addBastket(product){
+    let button = document.getElementById(product._id);
+    button.addEventListener("click", () => {
+        let productTable = JSON.parse(localStorage.getItem("produit"))
+        let options = document.getElementById("colors");
+        console.log(productTable)
+    })
 }
 
-addToCart.onclick = () =>{
-    localStorage.setItem("nom", price.value)
-}
+
+
+
+// addToCart.onclick = () =>{
+//     localStorage.setItem("nom", price.value)
+// }
