1b531841b8f7ba0bb6fdbdabef5dbe53cdc83a02
06480 <paul.messiant@gmail.com>
2022-05-17 18:36:55 +0200

    modifs

--- back/cart.js
+++ back/cart.js
@@ -2,10 +2,13 @@
 
 let Basket = JSON.parse(localStorage.getItem('product'));
 
+
+
 const displayBasket = async () => {
   if (Basket) {
     await Basket;
-    console.log('ok', Basket)
+
+
     
     cart__items.innerHTML = Basket.map((product) =>` 
     <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
@@ -21,7 +24,7 @@ const displayBasket = async () => {
       <div class="cart__item__content__settings">
         <div class="cart__item__content__settings__quantity">
           <p>Qté : </p>
-          <input type="number" change="change($event)" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+          <input type="number" id="id-input-test" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
         </div>
         <div class="cart__item__content__settings__delete">
           <p class="deleteItem" data-id="${product._id}" data-color="${product.color}">Supprimer</p>
@@ -31,10 +34,18 @@ const displayBasket = async () => {
   </article>
     `).join("");
 
+    const input = document.getElementById('id-input-test');
+    input.addEventListener('input', updateValue);
+    
+    function updateValue(e) {
+      console.log(e.target.value);
+    }
     cart__price.innerHTML = Basket.map((product) => `
-    <p>Total (<span id="totalQuantity">${product.quantity}</span> articles) : <span id="totalPrice">${product.quantity * product.price}</span> €</p>
+    <p>Total (<span id="totalQuantity">${product.quantity}</span> articles) : <span id="totalPrice">${input.value * product.price}</span> €</p>
     `).join("");
   }
+
+ 
 };
 
 displayBasket();
@@ -43,20 +54,17 @@ displayBasket();
 const deleteItems = async (displayBasket) => {
   await displayBasket;
   let remove = document.querySelectorAll(".deleteItem");
-  console.log('dacc', remove);
   remove.forEach((supprimer) => { 
     supprimer.addEventListener("click",() => {
-      console.log(supprimer);
+      
 
       let totalBasket = Basket.length;
-      console.log('mort', totalBasket);
 
       for(i = 0; i < totalBasket; i++){
         if(Basket[i].quantity >= 1 && totalBasket == 1 ){
           return (
            localStorage.removeItem("product"),
-            // (location.href = "cart.html"),
-            console.log("morty")
+            (location.href = "cart.html")
            );
         };
         if(Basket[i].quantity >= 1 && totalBasket != 1 
@@ -64,8 +72,7 @@ const deleteItems = async (displayBasket) => {
           && Basket[i].color == supprimer.dataset.color){
           Basket.splice([i], 1);
           localStorage.setItem("product", JSON.stringify(Basket));
-          // location.href = "cart.html";
-          console.log("moins 1");
+          location.href = "cart.html";
         }
         // if (Basket[i]._id == supprimer.dataset.id && Basket[i].color == supprimer.dataset.color){
         //   return (
@@ -80,19 +87,15 @@ const deleteItems = async (displayBasket) => {
 
 deleteItems();
 
-// selection de la quantitée du produit
-// const selectQuantity = document.querySelectorAll(".cart__item__content__settings__quantity");
-// console.log(selectQuantity);
 
-// function getQuantity(){
-// selectQuantity.addEventListener("change", (event) => {
-//     console.log(event.value);
-// });
-// }
+// selection de la quantitée du produit
+
+
+
+
 
 // vérification des données du formulaire
 let form = document.getElementById('Form');
-console.log(form)
 
 form.addEventListener('submit', () => {
   let input = document.getElementById('firstName');
@@ -122,7 +125,5 @@ let btnSendForm = document.getElementById("order")
 
 btnSendForm.addEventListener("click", () => {
 localStorage.setItem("Prénom", document.getElementById("firstname").value);
-
-  console.log(document.getElementById("firstname").value);
 
 });
